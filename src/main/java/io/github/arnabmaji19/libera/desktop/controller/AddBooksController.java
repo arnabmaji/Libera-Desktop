be9646bd98b98674a9b2319fb0ee578fae8b7413
Arnab Maji <arnabmaji1999@gmail.com>
2020-05-23 10:58:04 +0530

    add form validations for adding new book

--- src/main/java/io/github/arnabmaji19/libera/desktop/controller/AddBooksController.java
+++ src/main/java/io/github/arnabmaji19/libera/desktop/controller/AddBooksController.java
@@ -4,6 +4,7 @@ import io.github.arnabmaji19.libera.desktop.datasource.AuthorRequest;
 import io.github.arnabmaji19.libera.desktop.datasource.PublisherRequest;
 import io.github.arnabmaji19.libera.desktop.model.Author;
 import io.github.arnabmaji19.libera.desktop.model.Publisher;
+import io.github.arnabmaji19.libera.desktop.util.AlertDialog;
 import io.github.arnabmaji19.libera.desktop.util.AutoCompleteComboBoxListener;
 import io.github.arnabmaji19.libera.desktop.util.Validations;
 import javafx.application.Platform;
@@ -28,22 +29,30 @@ public class AddBooksController implements Initializable {
     @FXML
     private TextField yearPublishedTextField;
 
+    private AlertDialog alertDialog;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        this.alertDialog = new AlertDialog();
 
         // fetch authors for combobox
         var fetchAuthors = AuthorRequest
                 .getInstance()
                 .get()
-                .thenAcceptAsync(authors -> authorComboBox.setItems(FXCollections.observableArrayList(authors)));
+                .thenAcceptAsync(authors -> authorComboBox
+                        .setItems(FXCollections.observableArrayList(authors))
+                );
 
         // fetch publishers for combobox
         var fetchPublishers = PublisherRequest
                 .getInstance()
                 .get()
-                .thenAcceptAsync(publishers -> publisherComboBox.setItems(FXCollections.observableArrayList(publishers)));
+                .thenAcceptAsync(publishers -> publisherComboBox
+                        .setItems(FXCollections.observableArrayList(publishers))
+                );
 
-
+        // populate author and publisher combobox with data
         CompletableFuture
                 .allOf(fetchAuthors, fetchPublishers)
                 .thenRun(() -> Platform.runLater(() -> {
@@ -61,12 +70,27 @@ public class AddBooksController implements Initializable {
         var publisher = publisherComboBox.getValue();
         var yearPublished = yearPublishedTextField.getText();
 
-        if (title.isBlank() ||
-                author == null ||
-                publisher == null ||
-                !Validations.isNumber(yearPublished)) {
-            System.out.println("ERROR");
+        var errorMessage = validateFields(title, author, publisher, yearPublished);
+        if (!errorMessage.isEmpty()) {
+            alertDialog.show(errorMessage);
+            return;
         }
+        // TODO: make an http request to add new book
+    }
+
+    private String validateFields(String title,
+                                  Author author,
+                                  Publisher publisher,
+                                  String yearPublished) {
+        /*
+         * Validate Book Fields
+         */
+
+        if (title.isBlank()) return "Title can't be blank!";
+        if (author == null) return "Please select a valid Author!";
+        if (publisher == null) return "Please select a valid Publisher!";
+        if (!Validations.isNumber(yearPublished)) return "Year can't be a String!";
+        return "";
     }
 
 
--- src/main/java/io/github/arnabmaji19/libera/desktop/util/AlertDialog.java
+++ src/main/java/io/github/arnabmaji19/libera/desktop/util/AlertDialog.java
@@ -2,20 +2,22 @@ package io.github.arnabmaji19.libera.desktop.util;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import jfxtras.styles.jmetro.FlatAlert;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
 
 public class AlertDialog {
 
-    private JMetro jMetro;
-    private FlatAlert alert;
+    private final FlatAlert alert;
 
     public AlertDialog() {
-        this.jMetro = new JMetro(Style.LIGHT);
+        JMetro jMetro = new JMetro(Style.LIGHT);
         this.alert = new FlatAlert(Alert.AlertType.NONE);
         this.alert.getButtonTypes().setAll(ButtonType.OK);
-        this.jMetro.setScene(alert.getDialogPane().getScene());
+        jMetro.setScene(alert.getDialogPane().getScene());
+        ((Stage) alert.getDialogPane().getScene().getWindow()).initStyle(StageStyle.UNDECORATED);
     }
 
     public void show(String message) {
